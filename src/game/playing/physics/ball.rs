d64455415feaafa632d20190d06ca9e6a6f2c971
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-05-29 13:48:16 -0700

    Fixed the ball
    I feel like the physics actually feels better than in the old client

--- src/game/playing/physics/ball.rs
+++ src/game/playing/physics/ball.rs
@@ -10,9 +10,10 @@ pub fn physics_handle(
 ) {
     velocity_y.set(velocity_y.get() + gravity.get());
 
-    if *on_ground && (is_mouse_button_down(MouseButton::Left) || is_key_down(KeyCode::Space) || is_key_down(KeyCode::Up)) {
+    if *on_ground && (is_mouse_button_pressed(MouseButton::Left) || is_key_pressed(KeyCode::Space) || is_key_pressed(KeyCode::Up)) {
         gravity.set(-gravity.get());
         *player_y = if gravity.get() > 0.0 { *player_y + 1.0 } else { *player_y - 1.0 };
+        velocity_y.set(if gravity.get() > 0.0 { 5.0 } else { -5.0 });
         *on_ground = false;
     }
 }
