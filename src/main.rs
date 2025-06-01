2101308d07c02e4d1f2549391ac0f8e7580bc581
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-05-31 22:29:29 -0700

    guys look fyre is out
    are you proud of me

--- src/main.rs
+++ src/main.rs
@@ -442,7 +442,7 @@ async fn main() {
     let mut level_options_type: u8 = 1;
 
     println!("Getting latest version...");
-    let version: &str = "F-BETA";
+    let version: &str = "F-1.0";
     let level_version: &str = "F-ALPHA";
     let latest_version: String = ureq::get(latest_version_url)
         .query("fyre", "fyre")
