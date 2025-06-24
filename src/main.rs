933e6ba1360f22582d53367533399283e0ce72b0
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-06-23 23:05:50 -0700

    Changed version number
    I have no reason not to release my changes as an update so imma do that.

--- src/main.rs
+++ src/main.rs
@@ -486,7 +486,7 @@ async fn main() {
     let mut level_options_type: u8 = 1;
 
     println!("Getting latest version...");
-    let version: &str = "1.1.1";
+    let version: &str = "1.1.2";
     let level_version: &str = "F-ALPHA";
     let latest_version: String = ureq::get(latest_version_url)
         .query("fyre", "fyre")
